29a32bd72781fbedd5c5e86e1c072ad441076804
Kyle Wilcox <kwilcox@asascience.com>
2013-08-08 12:53:03 -0400

    Proxy through the new SKIPS global available in thredds_crawler. Add debug.

--- metadown/collectors/thredds.py
+++ metadown/collectors/thredds.py
@@ -5,12 +5,16 @@ import requests
 from thredds_crawler.crawl import Crawl
 
 class ThreddsCollector(object):
-    def __init__(self, url, selects=None, skips=None):
+
+    SKIPS = Crawl.SKIPS
+
+    def __init__(self, url, selects=None, skips=None, debug=None):
         self.selects    = selects
         self.skips      = skips
         self.url        = url
+        self.debug      = debug
 
     def run(self):
-        c = Crawl(self.url, self.selects, self.skips)
+        c = Crawl(self.url, self.selects, self.skips, self.debug)
         urls = ["%s?dataset=%s&catalog=%s" % (s.get("url"), d.id, d.catalog_url) for d in c.datasets for s in d.services if s.get("service").lower() == "iso"]
         return urls
